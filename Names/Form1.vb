3da5e7d513527891d8156509455adb5069532d62
Nillzie <nillzie@live.com.au>
2014-03-27 12:05:06 +0800

    Name Added

--- Names/Form1.vb
+++ Names/Form1.vb
@@ -4,6 +4,7 @@
 
         lstNames.Items.Add("Rayne")
         lstNames.Items.Add("Alan")
+        lstNames.Items.Add("Sean")
         'copy the line below and change it to your name
         'lstNames.Items.Add("")
 
